702f2e48c12888c49477539de19b5bbf008c5fe2
Ethan Hocking <eshocking253@gmail.com>
2020-05-05 21:56:09 -0600

    Change formatting for arrays of symbols to be preferred

--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,65 @@
 Rails.application.routes.draw do
-  #welcome
+  # welcome
   root 'welcome#index'
 
-  #merchants
+  # merchants
   resources :merchants do
-    #merchant items
-    resources :items, only: [:index, :new, :create], controller: :merchant_items
+    # merchant items
+    resources :items, only: %i[index new create], controller: :merchant_items
   end
 
-  #items
-  resources :items, except: [:new, :create] do
-      #item_reviews
-      resources :reviews, only: [:new, :create]
+  # items
+  resources :items, except: %i[new create] do
+    # item_reviews
+    resources :reviews, only: %i[new create]
   end
 
-  #item_reviews
-  resources :reviews, only: [:edit, :update, :destroy]
+  # item_reviews
+  resources :reviews, only: %i[edit update destroy]
 
-  #cart
+  # cart
   post '/cart/:item_id', to: 'cart#add_item'
   get '/cart', to: 'cart#show'
   delete '/cart', to: 'cart#empty'
   delete '/cart/:item_id', to: 'cart#remove_item'
   patch '/cart/:item_id', to: 'cart#add_or_sub'
 
-  #orders
-  resources :orders, only: [:new, :create, :show, :update]
+  # orders
+  resources :orders, only: %i[new create show update]
 
-  #register
+  # register
   get '/register', to: 'register#new'
   post '/register', to: 'register#create'
 
-  #login
+  # login
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
 
-  #logout
+  # logout
   delete '/logout', to: 'session#destroy'
 
-  #password_reset
-  resource :password, only: [:edit, :update]
+  # password_reset
+  resource :password, only: %i[edit update]
 
   namespace :profile, resources: :profile do
-    resources :orders, only: [:index, :show, :update]
+    resources :orders, only: %i[index show update]
     get '/', to: 'dashboard#show'
     get '/edit', to: 'dashboard#edit'
     patch '/', to: 'dashboard#update'
   end
 
-  #admin_user
+  # admin_user
   namespace :admin do
     get '/', to: 'dashboard#show'
-    resources :merchants, only: [:index, :show, :update]
-    resources :users, only: [:index, :show]
+    resources :merchants, only: %i[index show update]
+    resources :users, only: %i[index show]
   end
 
-  #merchant_user
+  # merchant_user
   namespace :merchant do
     get '/', to: 'dashboard#show'
     get '/orders/:id', to: 'orders#show'
-    resources :items, except: [:show]
-    resources :item_orders, only: [:update]
+    resources :items, except: %i[show]
+    resources :item_orders, only: %i[update]
   end
 end
